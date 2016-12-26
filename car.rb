7462e1d1b6acfb2d9ea314d82a301fdaf1621799
vrinek <vrinek@users.noreply.github.com>
2016-12-26 22:07:49 +0000

    Extract all car logic into new Car class

---
+++ car.rb
@@ -0,0 +1,69 @@
+class Car
+  SPRITE_SCALE = 0.3
+
+  LINEAR_ACCELERATION = 0.15
+  BREAKING_POWER = 0.3
+  LINEAR_FRICTION = 0.98
+  # Top speed = 0.15 / (1 - 0.98) * 0.98 = 7.35px/fr
+
+  ANGULAR_ACCELERATION = 0.07
+  ANGULAR_FRICTION = 0.9
+  # Top angular velocity = 0.07 / (1 - 0.9) * 0.9 = 0.63deg/fr
+
+  def initialize(x, y)
+    @sprite = Gosu::Image.new('car.png')
+    @velocity = 0
+    @angle_velocity = 0
+    @x = x
+    @y = y
+    @angle = 0
+  end
+
+  def update
+    decrease_angular_velocity if Gosu.button_down? Gosu::KbLeft
+    increase_angular_velocity if Gosu.button_down? Gosu::KbRight
+    accelerate                if Gosu.button_down? Gosu::KbUp
+    brake                     if Gosu.button_down? Gosu::KbDown
+
+    turn
+    move
+  end
+
+  def draw
+    @sprite.draw_rot(@x, @y, 1, @angle, 0.5, 0.6, SPRITE_SCALE, SPRITE_SCALE)
+  end
+
+  private
+
+  def increase_angular_velocity
+    @angle_velocity += ANGULAR_ACCELERATION
+  end
+
+  def decrease_angular_velocity
+    @angle_velocity -= ANGULAR_ACCELERATION
+  end
+
+  def accelerate
+    @velocity += LINEAR_ACCELERATION
+  end
+
+  def brake
+    @velocity -= BREAKING_POWER
+
+    @velocity = [@velocity, 0].max
+  end
+
+  def move
+    @x += Gosu.offset_x(@angle, @velocity)
+    @y += Gosu.offset_y(@angle, @velocity)
+    @x %= 640
+    @y %= 480
+
+    @velocity *= LINEAR_FRICTION
+  end
+
+  def turn
+    @angle += @angle_velocity * @velocity
+    @angle_velocity *= ANGULAR_FRICTION
+  end
+end
--- game.rb
+++ game.rb
@@ -1,78 +1,25 @@
 require 'gosu'
 
+require_relative './car.rb'
+
+# Main game window
 class GameWindow < Gosu::Window
-  CAR_SCALE = 0.3
-
-  LINEAR_ACCELERATION = 0.15
-  BREAKING_POWER = 0.3
-  LINEAR_FRICTION = 0.98
-  # Top speed = 0.15 / (1 - 0.98) * 0.98 = 7.35px/fr
-
-  ANGULAR_ACCELERATION = 0.07
-  ANGULAR_FRICTION = 0.9
-  # Top angular velocity = 0.07 / (1 - 0.9) * 0.9 = 0.63deg/fr
+  WIDTH = 640
+  HEIGHT = 480
 
   def initialize
-    super 640, 480
+    super(WIDTH, HEIGHT)
     self.caption = 'Gosu Tutorial Game'
 
-    @car = Gosu::Image.new('car.png')
-    @velocity = 0
-    @angle_velocity = 0
-    @x = 320
-    @y = 240
-    @angle = 0
+    @car = Car.new(WIDTH / 2, HEIGHT / 2)
   end
 
   def update
-    if Gosu.button_down? Gosu::KbLeft
-      @angle_velocity -= ANGULAR_ACCELERATION
-    end
-
-    if Gosu.button_down? Gosu::KbRight
-      @angle_velocity += ANGULAR_ACCELERATION
-    end
-
-    if Gosu.button_down? Gosu::KbUp
-      accelerate
-    end
-
-    if Gosu.button_down? Gosu::KbDown
-      brake
-    end
-
-    turn
-    move
+    @car.update
   end
 
   def draw
-    @car.draw_rot(@x, @y, 1, @angle, 0.5, 0.6, CAR_SCALE, CAR_SCALE)
-  end
-
-  private
-
-  def accelerate
-    @velocity += LINEAR_ACCELERATION
-  end
-
-  def brake
-    @velocity -= BREAKING_POWER
-
-    @velocity = [@velocity, 0].max
-  end
-
-  def move
-    @x += Gosu.offset_x(@angle, @velocity)
-    @y += Gosu.offset_y(@angle, @velocity)
-    @x %= 640
-    @y %= 480
-
-    @velocity *= LINEAR_FRICTION
-  end
-
-  def turn
-    @angle += @angle_velocity * @velocity
-    @angle_velocity *= ANGULAR_FRICTION
+    @car.draw
   end
 end
 
